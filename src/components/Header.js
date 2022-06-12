1255d0766cdb46f09c4d7c0bfb98bd5dead9bc06
Gaurav Rozatkar <gauravrozatkar1@gmail.com>
2022-06-12 17:14:23 +0530

    Implemented:
    Because:

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import logo from "../images/logo/logo.svg";
+import "../css/Header.css";
+import hamburger from "../images/icons/hamburger.svg";
+import close from "../images/icons/close.svg";
+
+function Header() {
+  return (
+    <nav>
+      <div class="Brand">
+        <img src={logo} className="App-logo" aslt="logo" />
+        <div class="Brand-name"> Sofaseth </div>
+      </div>
+      <img className="Hamburger-icon" src={hamburger} ></img>
+     <ul>
+        <li> Sofas </li> <li> Orders </li> <li> Cart(0) </li> <li> Account </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
